2182335fd86b56c5dc47cf7263028453b639aeb0
Leigh Simpson <code@simpleigh.com>
2017-12-25 22:25:52 +0000

    Stopped maintaining config state outside the created middleware

--- src/index.js
+++ src/index.js
@@ -2,23 +2,15 @@ import Ajv from 'ajv';
 
 import { standardActionSchema, fluxStandardActionSchema } from './defaults';
 
-const middlewareConfig = {
-  actionSchema: { },
-  perActionSchemas: { }
-};
-
 export default (config = { }) => {
   const ajv = new Ajv();
 
-  ['actionSchema', 'perActionSchemas'].forEach(key => {
-    if (config[key]) {
-      middlewareConfig[key] = config[key];
-    }
-  });
-
   ajv.addSchema(standardActionSchema, 'standardActionSchema');
   ajv.addSchema(fluxStandardActionSchema, 'fluxStandardActionSchema');
-  ajv.addSchema(middlewareConfig.actionSchema, 'actionSchema');
+
+  if (config.actionSchema) {
+    ajv.addSchema(config.actionSchema, 'actionSchema');
+  }
 
   for (let type in config.perActionSchemas) {
     ajv.addSchema(config.perActionSchemas[type], `action/${type}`);
@@ -35,11 +27,11 @@ export default (config = { }) => {
       }
     }
 
-    if (!ajv.validate('actionSchema', action)) {
+    if (config.actionSchema && !ajv.validate('actionSchema', action)) {
       throw new Error(ajv.errorsText(ajv.errors));
     }
 
-    if (middlewareConfig.perActionSchemas[action.type]) {
+    if (config.perActionSchemas && config.perActionSchemas[action.type]) {
       if (!ajv.validate(`action/${action.type}`, action)) {
         throw new Error(ajv.errorsText(ajv.errors));
       }
